a179c697021ea073576dba3b81de80fed629431e
Joel <camacho-joel@hotmail.com>
2019-02-22 23:58:26 +0100

    First commit Minicms webiste

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/api/pages/:page", (req, res) => {
+  const fileName = `data/${req.params.page}.md`;
+  fs.readFile(fileName, "utf-8", (err, data) => {
+    if (err) {
+      res.send({ status: "error", error: err.toString() });
+    } else {
+      const lines = data.split("\n");
+      const title = lines[0].substring(2);
+      const body = lines
+        .slice(1)
+        .join("\n")
+        .trim();
+      const page = { title, body };
+      res.send({ status: "ok", page });
+    }
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http.localhost:${PORT}`);
+});
